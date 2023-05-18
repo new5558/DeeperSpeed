1f993b740b49cd1e0cc18a5009194d927ddc4fa6
new5558 <new17353@gmail.com>
2023-05-18 22:50:29 +0700

    fix: no key host key check f

--- deepspeed/launcher/multinode_runner.py
+++ deepspeed/launcher/multinode_runner.py
@@ -72,8 +72,8 @@ class PDSHRunner(MultiNodeRunner):
         # See https://linux.die.net/man/1/pdsh for flag details
         # import os
         # os.environ["PDSH_SSH_ARGS_APPEND"] = "-o StrictHostKeyChecking=no"
+        environment['PDSH_SSH_ARGS_APPEND'] = '-o StrictHostKeyChecking=no'
         pdsh_cmd_args = [
-            'export PDSH_SSH_ARGS_APPEND="-o StrictHostKeyChecking=no" &&'
             'pdsh',
             '-S',
             '-f',
